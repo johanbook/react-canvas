1b0b03c510ddcba114777e6f52601a4de5654221
Johan Book <johan.book@desupervised.io>
2020-10-27 21:53:24 +0100

    docs: generate docs

---
+++ docs/precache-manifest.f8556ac0f45adf42c24c9f37556ea138.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ccdf534d91d9ed51d91",
+    "url": "build/2.5ccdf534.js"
+  },
+  {
+    "revision": "bf100c848509fd0333d023327fff8ded",
+    "url": "build/2.5ccdf534.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c9ee50ab8b78dc2e68",
+    "url": "build/bundle.d9c9ee50.js"
+  },
+  {
+    "revision": "202c8ac447c0fac6d003",
+    "url": "build/main.202c8ac4.js"
+  },
+  {
+    "revision": "a744e6d099dfc360903a9378fa064734",
+    "url": "index.html"
+  }
+]);
