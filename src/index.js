3ad41307d962c96be59560c24466b56e62fd88e5
Johan Book <johan.book@desupervised.io>
2020-10-31 20:27:52 +0100

    feat: add createItems utility function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 export { default as Canvas } from "./Canvas";
 export { default } from "./ResponsiveCanvas";
+
+export function createItems(number) {
+  const items = [];
+  for (let idx = 0; idx < number; idx++) {
+    items.push({ x: idx / number, y: 0 });
+  }
+
+  return items;
+}
